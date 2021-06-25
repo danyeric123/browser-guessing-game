ef3e4c4d0c4e33d2ba01e8c5f943efb4fc488986
danyeric123 <dnagarpowers@gmail.com>
2021-06-25 14:21:21 -0400

    prevGuesses and getGuess functional

--- js/app.js
+++ js/app.js
@@ -2,9 +2,15 @@ const game = {
     title: 'Guess the Number!',
     biggestNum: 100,
     smallestNum: 1,
+    prevGuesses: [],
     secretNum: null,
+    getGuess: function(){
+        prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`)
+    },
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     }
   }
+
+  console.log(game.getGuess())
