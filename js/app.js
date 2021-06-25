f9921459761c40ec9929b4612bd0f6dabedb3e49
danyeric123 <dnagarpowers@gmail.com>
2021-06-25 16:24:03 -0400

    all functions work

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
 const game = {
-    title: 'Guess the Number!',
-    biggestNum: 100,
-    smallestNum: 1,
-    prevGuesses: [],
-    secretNum: null,
-    getGuess: function(){
-        let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`)
-        while(Number.isInteger(parseInt(guess))?
-              parseInt(guess)<this.smallestNum||parseInt(guess)>this.biggestNum
-              :true){
-          //console.log(Number.isInteger(parseInt(guess)))
-          guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`)
-        }
-        return guess
-    },
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-      // this.secretNum = 42
-      let guess = null
-      while(guess!=this.secretNum){
-        // console.log(this.prevGuesses)
-        guess=this.getGuess()
-        this.prevGuesses.push(guess)
-        this.render(guess)
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  prevGuesses: [],
+  secretNum: null,
+  getGuess: function(){
+      let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`)
+      while(Number.isInteger(parseInt(guess))?
+            parseInt(guess)<this.smallestNum||parseInt(guess)>this.biggestNum
+            :true){
+        //console.log(Number.isInteger(parseInt(guess)))
+        guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`)
       }
-      return
-    },
-    render: function(presentGuess){
-      if(presentGuess==this.secretNum){
-        alert(`Congrats! You guessed the number in ${this.prevGuesses.length}!`)
-      }else{
-        alert(`Your guess is too ${presentGuess>this.secretNum?"high":"low"} 
-        Previous guesses: ${this.prevGuesses.join(", ")}`)
-      }
+      return guess
+  },
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+    // this.secretNum = 42
+    let guess = null
+    while(guess!=this.secretNum){
+      // console.log(this.prevGuesses)
+      guess=this.getGuess()
+      this.prevGuesses.push(guess)
+      this.render(guess)
+    }
+    return
+  },
+  render: function(presentGuess){
+    if(presentGuess==this.secretNum){
+      alert(`Congrats! You guessed the number in ${this.prevGuesses.length}!`)
+    }else{
+      alert(`Your guess is too ${presentGuess>this.secretNum?"high":"low"} 
+      Previous guesses: ${this.prevGuesses.join(", ")}`)
     }
   }
+}
 
-  game.play()
+game.play()
