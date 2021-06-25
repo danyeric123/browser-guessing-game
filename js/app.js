45d2c1dc372762af336ef43bc4aac81521028cc0
danyeric123 <dnagarpowers@gmail.com>
2021-06-25 15:32:36 -0400

    play invokes getGuess

--- js/app.js
+++ js/app.js
@@ -9,14 +9,21 @@ const game = {
         while(Number.isInteger(parseInt(guess))?
               parseInt(guess)<this.smallestNum||parseInt(guess)>this.biggestNum
               :true){
-          console.log(Number.isInteger(parseInt(guess)))
-          guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`)
+          //console.log(Number.isInteger(parseInt(guess)))
+          guess = prompt(`Enter a ${this.secretNum} guess between ${this.smallestNum} and ${this.biggestNum}:`)
         }
+        return guess
     },
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+      //console.log(this.secretNum)
+      let guess = this.getGuess()
+      while(guess!=this.secretNum){
+        this.prevGuesses.push(guess)
+        guess=this.getGuess()
+      }
     }
   }
 
-  game.getGuess()
+  game.play()
